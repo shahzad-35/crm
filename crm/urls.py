bf3c0bd1727f2f045a9893901e05fc17e5f6f62a
Shahzad <shahzadhere17@gmail.com>
2023-02-15 23:55:14 +0500

    added routes and created functions to display sample data

--- crm/urls.py
+++ crm/urls.py
@@ -16,6 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def contactUs(request):
+    return HttpResponse("Contact us")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('contact-us/', contactUs),
 ]
