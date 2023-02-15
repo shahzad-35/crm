25a67bd2236c2a924680d5d5b97eb2e20d04f30d
Shahzad <shahzadhere17@gmail.com>
2023-02-16 00:09:55 +0500

    redirection to routes of different apps from main url file

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home Page")
+
+def products(request):
+    return HttpResponse("Products")
+
+def customer(request):
+    return HttpResponse("customer")
+
+def contactUs(request):
+    return HttpResponse("contact us")
